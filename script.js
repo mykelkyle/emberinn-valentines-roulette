0974c75c606e62a0d9d0109b83be1699c9c954ee
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2023-02-14 09:25:36 +0100

    Refactor fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const input = [
+const members = [
   "Kuro",
   "Anna",
   "Lucy",
@@ -32,19 +32,18 @@ const input = [
 ];
 
 const startBtn = document.getElementById("start-button");
-const nameContainer = document.querySelector(".name-container");
-const memberContainer = document.querySelector(".member-container");
-const btnContainer = document.querySelector(".button-container");
+const nameContainer = document.getElementById("name-container");
+const memberContainer = document.getElementById("member-container");
+const btnContainer = document.getElementById("button-container");
 const requiredChoices = 3;
 const extraChoices = 3;
-let counter = 0;
 let nameArr = [];
 
 function renderChoices() {
-  const randomNames = getRandomNames(input, requiredChoices)
-  for (const [number, item] of randomNames.entries()) {
-	document.querySelector(".choice-button-" + number).textContent = item;
-  }
+  const randomNames = getRandomNames(members, requiredChoices);
+  document.querySelectorAll(".choice-button").forEach((choiceBtn, i) => {
+    choiceBtn.textContent = randomNames[i];
+  });
 }
 
 function addName(name) {
@@ -55,44 +54,41 @@ function addName(name) {
 }
 
 function renderMembers() {
-  input.filter(name => !nameArr.includes(name)).forEach(item, i => {
+  for (const member of members) {
+    if (nameArr.includes(member)) continue;
+
     const memberBtn = document.createElement("button");
     memberBtn.setAttribute("class", "member-button");
+    memberBtn.setAttribute("onclick", "clickMember(this)");
     memberContainer.appendChild(memberBtn);
-    memberBtn.textContent = name;
-  })
+    memberBtn.textContent = member;
+  }
 }
 
-//  Start button
-startBtn.addEventListener("click", () => {
-  startBtn.classList.add("hidden");
-  for (i = 0; i < requiredChoices; i++) {
-	const choiceBtn = document.createElement("button").setAttribute("id", "choice-button-" + i);
-	btnContainer.appendChild(choiceBtn);
+function clickStart() {
+  startBtn.setAttribute("class", "hidden");
+  for (const _ of [...Array(requiredChoices)]) {
+    const choiceBtn = document.createElement("button");
+    choiceBtn.setAttribute("class", "choice-button");
+    choiceBtn.setAttribute("onclick", "clickChoice(this)");
+    btnContainer.appendChild(choiceBtn);
   }
   renderChoices();
-});
-
-// Choice buttons
-for (i = 0; i < requiredChoices; i++) {
-	document.querySelector("choice-button-" + i).addEventListener("click", function (e) {
-		renderChoices();
-		addName(e.target.textContent);
-		if (nameArr.length >= requiredChoices) {
-			btnContainer.textContent = "";
-			renderMembers();
-		}
-	});
 }
 
-// Member button
-document.body.addEventListener("click", function (e) {
-  if (e.target.className == "member-button") {
-    e.target.classList.add("hidden");
-    addName(e.target.textContent);
+function clickChoice(e) {
+  addName(e.textContent);
+  renderChoices();
+  if (nameArr.length >= requiredChoices) {
+    btnContainer.textContent = "";
+    renderMembers();
   }
-});
+}
 
+function clickMember(e) {
+  e.setAttribute("class", "hidden");
+  addName(e.textContent);
+}
 
 // This method gets multiple random items from a list by using the Modern Fisher-Yates Shuffle method
 // This method makes duplicates impossible by putting "taken" numbers in the "shuffled part" of the list and only getting a new number from the "unshuffled part".
@@ -118,14 +114,14 @@ function getRandomNames(arr, n) {
 }
 
 function createChart() {
-	const chartBody = document.getElementById("name-chart-data");
-	for (item of input) {
-		const tr = document.createElement("tr");
-		const th = document.createElement("th");
-		th.textContent = item;
-		const td = document.createElement("td")
-		td.setAttribute("id", item);
-		td.setAttribute("style", "--size: calc( 0/6 )");
+  const chartBody = document.getElementById("name-chart-data");
+  for (item of members) {
+    const tr = document.createElement("tr");
+    const th = document.createElement("th");
+    th.textContent = item;
+    const td = document.createElement("td");
+    td.setAttribute("id", item);
+    td.setAttribute("style", "--size: calc( 0/6 )");
 
     tr.appendChild(th);
     tr.appendChild(td);
@@ -134,18 +130,18 @@ function createChart() {
 }
 
 function updateChart(name) {
-	const column = document.getElementById(name).style.cssText
-	const amount = parseInt(column.match(/\d/));
-	document.getElementById(name).style.cssText = column.replace(/\d/, amount + 1);
-	if (amount == 3 - 1) {
-		input.splice(input.indexOf(name), 1);
-	}
+  const column = document.getElementById(name).style.cssText;
+  const amount = parseInt(column.match(/\d/));
+  document.getElementById(name).style.cssText = column.replace(/\d/, amount + 1);
+  if (amount == 3 - 1) {
+    members.splice(members.indexOf(name), 1);
+  }
 }
 
-createChart(input);
+createChart(members);
 
-// const len = input.length * 3
+// const len = members.length * 3
 // for (let i = 0; i < len; i++) {
-//   const randomName = getRandomNames(input, 3)[Math.floor(Math.random() * 3)]
+//   const randomName = getRandomNames(members, 3)[Math.floor(Math.random() * 3)]
 //   updateChart(randomName);
 // }
