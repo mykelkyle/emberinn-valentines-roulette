3ab4b6dd29001a2ec08fd8c81b406ff2e97d457d
Mykel Murphy <murphy@akane.waseda.jp>
2023-02-16 11:29:00 -0500

    Format image placeholder for choices

--- script.js
+++ script.js
@@ -29,6 +29,10 @@ const input = [
   "Siwar",
 ];
 
+const pictures = [
+  "https://cdn.discordapp.com/avatars/250957620930805761/ff6bc719ccc1366340495e4ecb958cda.webp?size=160",
+];
+
 const startBtn = document.getElementById("start-button");
 const nameContainer = document.querySelector(".name-container");
 const memberContainer = document.querySelector(".member-container");
@@ -39,6 +43,12 @@ const instructionsContainer = document.getElementById("instructions-container");
 let counter = 0;
 let nameArr = [];
 
+function renderImage(container) {
+  const picture = document.createElement("img");
+  picture.setAttribute("src", pictures[0]);
+  container.appendChild(picture);
+}
+
 function renderChoices() {
   for (i = 0; i < 3; i++) {
     const choiceDiv = document.createElement("div");
@@ -47,6 +57,7 @@ function renderChoices() {
     choiceDiv.setAttribute("class", "choice-div");
     choiceBtn.setAttribute("id", "choice-button");
     btnContainer.appendChild(choiceDiv);
+    renderImage(choiceDiv);
     choiceDiv.appendChild(choiceBtn);
     choiceBtn.textContent += randomName;
   }
